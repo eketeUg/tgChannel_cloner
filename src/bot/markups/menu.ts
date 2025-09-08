dcd8b9e11babd3acb77435cdc8be12dadcd51f4a
eketeUg <emmanuelugochukwuekete@gmail.com>
2025-09-08 06:53:05 +0100

    fixed unique chatId

--- src/bot/markups/menu.ts
+++ src/bot/markups/menu.ts
@@ -4,7 +4,7 @@ export const menuMarkup = async () => {
     keyboard: [
       [
         {
-          text: 'clone channel 👀',
+          text: 'clone channel 🧬',
           callback_data: JSON.stringify({
             command: '/watchChannel',
             language: 'english',
